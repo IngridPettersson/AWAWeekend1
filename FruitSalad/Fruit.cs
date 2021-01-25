e2f07f80f4efad6b35d1a82dce38cdf61cfb96a0
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-01-25 12:54:40 +0100

    Add Max and Min methods

---
+++ FruitSalad/Fruit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FruitSalad
+{
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public Fruit(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ FruitSalad/ProgramFruits.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FruitSalad
+{
+    class ProgramFruits
+    {
+        static string fruitName;
+        static double fruitPrice;
+        static List<Fruit> fruitList = new List<Fruit>();
+        static Fruit fruit;
+        static double maxValue;
+        static double minValue;
+
+        static void Main(string[] args)
+        {
+            AddFruitsToList();
+            PrintOut();
+            maxValue = GetMaxValue(fruitList);
+            Console.WriteLine(maxValue);
+            minValue = GetMinValue(fruitList);
+            Console.WriteLine(minValue);
+        }
+
+        private static void AddFruitsToList()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+            Console.Write("Enter a fruit: ");
+            fruitName = Console.ReadLine();
+            Console.Write("Enter price of fruit: ");
+            fruitPrice = Convert.ToDouble(Console.ReadLine());
+
+            fruit = new Fruit(fruitName, fruitPrice);
+                fruitList.Add(fruit);
+            }
+        }
+
+        private static void PrintOut()
+        {
+            foreach (Fruit fruit in fruitList)
+            {
+                Console.WriteLine($"\nFruit: {fruit.Name}\nPrice: {fruit.Price}\n");
+            }
+        }
+
+        private static double GetMaxValue(List<Fruit> list)
+        {
+            maxValue = list.Max(r => r.Price);
+            return maxValue;
+        }
+
+        private static double GetMinValue(List<Fruit> list)
+        {
+            minValue = list.Min(r => r.Price);
+            return minValue;
+        }
+    }
+}
